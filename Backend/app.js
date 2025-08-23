da86628102f1b6a9e0513ed91813333743061893
Anshu9693 <anshukumarsah8585@gmail.com>
2025-08-24 01:15:44 +0530

    ready for render

--- Backend/app.js
+++ Backend/app.js
@@ -9,7 +9,7 @@ const userRoutes = require('./routes/user.routes')
 const captainRoutes = require('./routes/captain.routes')
 const mapsRoutes = require('./routes/maps.routes')
 const rideRoutes = require('./routes/ride.routes')
-
+const path = require('path');
 
 connectToDb()
 
@@ -31,5 +31,11 @@ app.use('/users',userRoutes)
 app.use('/captains',captainRoutes)
 app.use('/maps',mapsRoutes)
 app.use('/rides', rideRoutes)
+
+
+app.use(express.static(path.join(__dirname, '../Frontend/dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../Frontend/dist/index.html'));
+});
 
 module.exports = app; 
